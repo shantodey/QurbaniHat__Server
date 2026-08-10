9dc9b95a559db25432a779bf957341f341b21af3
shantodey <ratul3330@gmail.com>
2026-08-11 00:40:44 +0600

    feat: integrate better-auth for authentication and add user session management

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,32 @@
-import express, { Router } from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth"; 
 import router from "./routes/routes";
 
+dotenv.config();
+
+const app = express();
 
 
-const app=express()
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true, 
+  })
+);
 
 
-app.use(cors())
-app.use(express.json())
 
-app.use("/",router)
+app.all("/api/auth/*path", toNodeHandler(auth));
+app.use(express.json());
+app.use("/", router);
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome",
+  });
+});
 
-app.get('/',(req,res)=>{
-    res.json({
-        success:true,
-        massage:'Wellconme'
-    });
-})
-
-export default app
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import app from "./app";
+
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
 
-
-const PORT=process.env.PORT|| 5000
-
-app.listen(PORT,()=>{
-    console.log("welcome to the server")
+app.listen(PORT, () => {
+    console.log(`Server running on`);
 });
