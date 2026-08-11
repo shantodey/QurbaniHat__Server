8cac333389c708656f9c94578de2ffe3b2b3ec66
shantodey <ratul3330@gmail.com>
2026-08-12 03:03:16 +0600

    chore: simplify CORS middleware configuration and remove vercel.json

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,7 @@ dotenv.config();
 const app = express();
 
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true, 
-  })
-);
+app.use( cors({ origin: process.env.FRONTEND_URL, credentials: true,  }));
 
 
 
