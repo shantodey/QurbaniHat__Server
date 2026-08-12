481b80b7fbb5b987b9e4759ab1980d9a5dcff13b
shantodey <ratul3330@gmail.com>
2026-08-12 13:12:49 +0600

    feat: initialize application with Express server and update TypeScript configuration to ESNext module resolution

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth"; 
+import { auth } from "./lib/auth";
 import router from "./routes/routes";
 
 dotenv.config();
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 
-app.use( cors({ origin: process.env.FRONTEND_URL, credentials: true,  }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true, }));
 
 
 
